18a0c8b3724d3c040ffc993c75502349b79eb13f
Telakos <tel.akos.2@gmail.com>
2017-02-07 11:34:00 +0100

    fact skeleton done

--- Fact.java
+++ Fact.java
@@ -1,5 +1,27 @@
+import java.util.Set;
+
 /**
  * Created by akos on 2017.02.07..
  */
 public class Fact {
+    public Fact(String description)
+    {
+
+    }
+    public void setFactValueByID(String id, boolean value)
+    {
+
+    }
+    public Set<String> getIDSet()
+    {
+        return null;
+    }
+    public boolean getValueByID()
+    {
+        return false;
+    }
+    public String getDescription()
+    {
+        return null;
+    }
 }
