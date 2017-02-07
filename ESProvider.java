7c56b158c8f193a4f0e4dae1dad9da03b4352774
Telakos <tel.akos.2@gmail.com>
2017-02-07 11:27:43 +0100

    ESProvider skeleton done

--- ESProvider.java
+++ ESProvider.java
@@ -2,4 +2,21 @@
  * Created by akos on 2017.02.07..
  */
 public class ESProvider {
+    public ESProvider(FactParser factParser, RuleParser ruleParser)
+    {
+
+    }
+    public void collectAnswers()
+    {
+
+    }
+    public boolean getAnswerByQuestion(String QuestionID)
+    {
+        return false;
+    }
+    public String evaluate()
+    {
+        return null;
+    }
+
 }
