27ca8f545b88eaef2af1afce4bc26c9f5da55618
Telakos <tel.akos.2@gmail.com>
2017-02-08 13:40:17 +0100

    Started working on RuleRepository

--- RuleParser.java
+++ RuleParser.java
@@ -3,6 +3,8 @@ public class RuleParser extends XmlParser
 {
     public RuleRepository getRuleRepository()
     {
+        String filename = "";
+        loadXmlDocument(filename);
         RuleRepository rep = new RuleRepository();
         rep.addQuestion("1", null);
         return rep;
