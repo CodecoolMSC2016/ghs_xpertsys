9f9e0c7e8bc461b36ddf3992e056df952cdc55fa
Telakos <tel.akos.2@gmail.com>
2017-02-08 13:53:17 +0100

    removed an army of unnecessary inputs in XMLParser

--- XmlParser.java
+++ XmlParser.java
@@ -1,21 +1,3 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
 public abstract class XmlParser {
 
     public abstract void loadXmlDocument(String fullPath);
