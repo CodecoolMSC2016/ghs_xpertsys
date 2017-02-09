1d8870f35196f9a4c12cd15a96969cb00d145b7c
Telakos <tel.akos.2@gmail.com>
2017-02-09 17:39:28 +0100

    temp debug print method to ESProvider

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@
 public class Main {
     public static void main(String[] args) {
         ESProvider provider = new ESProvider(new FactParser(), new RuleParser());
+        provider.debugPrintStats();
     }
 
 
