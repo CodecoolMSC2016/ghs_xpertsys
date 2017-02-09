5308895d17993289257dcf338fc09995705ee406
Trixi <bozso.beatrix@gmail.com>
2017-02-09 15:20:40 +0100

    semicolon was missing

--- Fact.java
+++ Fact.java
@@ -1,23 +1,23 @@
-import java.util.Set;
+import java.util.*;
+
 
 /**
  * Created by akos on 2017.02.07..
  */
 public class Fact {
-    public Fact(String description)
-    {
+    public Fact(String description) {
 
     }
-    public void setFactValueByID(String id, boolean value)
-    {
+
+    public void setFactValueByID(String id, boolean value) {
         /* Display content using Iterator*/
         Set set = factMap.entrySet();
         Iterator iterator = set.iterator();
-        while(iterator.hasNext()) {
-            Map.Entry mentry = (Map.Entry)iterator.next();
-            System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
+        while (iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry) iterator.next();
+            System.out.print("key is: " + mentry.getKey() + " & Value is: ");
             System.out.println(mentry.getValue());
-    }
+        }
     public Set<String> getIDSet()
     {
         return null;
@@ -29,5 +29,6 @@ public class Fact {
     public String getDescription()
     {
         return null;
+    }*/
     }
 }
