6756c7b8c3e49d63574dbd4c60d4b7e1c596f18c
Trixi <bozso.beatrix@gmail.com>
2017-02-09 17:26:21 +0100

    FactParser finished, FactRepository finished

--- FactParser.java
+++ FactParser.java
@@ -1,4 +1,6 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -11,15 +13,52 @@ import java.io.FileInputStream;
 
 public class FactParser extends XmlParser
 {
+    String fileName = "Facts.xml";
+    private NodeList nodeList;
+
     public FactRepository getFactRepository()
     {
-        return null;
+        FactRepository factRepo = new FactRepository();
+
+        String description = "";
+        loadXmlDocument(fileName);
+
+
+        for(int i = 0; i < nodeList.getLength(); i++)
+        {
+
+            Node node = nodeList.item(i);
+            Node tempNode = ((Element) node).getElementsByTagName("description").item(0);
+            description = ((Element)tempNode).getAttribute("value");
+
+            String factId = ((Element)node).getAttribute("id");
+
+            Fact fact = new Fact(description);
+            fact.setId(factId);
+            parseEvals(node, fact);
+
+            factRepo.addFact(fact);
+        }
+        return factRepo;
+    }
+
+    public void parseEvals(Node node, Fact fact)
+    {
+        NodeList evalList = ((Element)((Element)node).getElementsByTagName("Evals").item(0))
+                .getElementsByTagName("Eval");
+        for (int i = 0; i < evalList.getLength(); i++)
+        {
+            Node evalNode = evalList.item(i);
+            String evalId = ((Element) evalNode).getAttribute("id");
+            Boolean evalValue = Boolean.parseBoolean(evalNode.getTextContent());
+
+            fact.setFactValueByID(evalId, evalValue);
+        }
     }
 
     @Override
     public void loadXmlDocument(String fullPath)
     {
-        fullPath = "Home/Java/xpertSystem/Facts.xml";
 
         try
         {
@@ -30,6 +69,8 @@ public class FactParser extends XmlParser
 
             xmlDocument.getDocumentElement().normalize();
 
+            NodeList nodeList = xmlDocument.getElementsByTagName("Fact");
+
         }
         catch (Exception e)
         {
@@ -37,9 +78,6 @@ public class FactParser extends XmlParser
         }
     }
 
-    public void parseXml(String fileName)
-    {
-        Document doc = loadXmlDocument(fileName);
-    }
+
 }
 
